d99d30f22faeb2cda1c91b3a1fd4958e75abefc8
김영준 <j053okok@gmail.com>
2023-08-09 11:35:37 +0900

    readme 내용 추가

---
+++ Quest1/Dice.py
@@ -0,0 +1,26 @@
+import random
+
+
+class FunnyDice:
+    def __init__(self, n=6):  # n의 값을 입력하지 않을 시 기본값은 6
+        self.n = int(n)  # 정수 처리
+        self.val = random.randrange(1, self.n + 1)  # 1 <= n < (n + 1) 최종 출력값 밸류에 바로 랜덤기능을 적용
+
+    def getval(self):
+        return self.val  # getter기능을 사용해서 외부에서 이 클래스에 접근할 때 해당 메서드를 사용하게 함
+
+    def setval(self, val):
+        if val <= 0:
+            raise ValueError("Invalid number")
+        elif val < self.n:
+            self.val = val
+
+
+def throw():
+    n = int(input())
+    dice = FunnyDice(n)
+    print(f"행운의 숫자는? {dice.getval()}")
+
+
+if __name__ == '__main__':
+    throw()
