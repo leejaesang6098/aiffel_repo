b9deed09f6c4772093f8b010984d078ed9035ebc
j053okok <137843189+j053okok@users.noreply.github.com>
2023-08-09 14:44:27 +0900

    input 텍스트입력 추가

--- Quest1/Dice.py
+++ Quest1/Dice.py
@@ -1,6 +1,7 @@
 import random
 
 
+# 현재 코드의 컨셉은 불필요한 부분을 모두 날리고 한줄이라도 줄이려 함
 class FunnyDice:
     def __init__(self, n=6):  # n의 값을 입력하지 않을 시 기본값은 6
         self.n = int(n)  # 정수 처리
@@ -11,10 +12,10 @@ class FunnyDice:
 
 
 def throw():  # throw 함수와 main 함수를 합침
-    n = int(input())
+    n = int(input("주사위 면의 갯수를 입력하세요: "))
     dice = FunnyDice(n)
     print(f"행운의 숫자는? {dice.getval()}")
 
 
-if __name__ == '__main__':
-    throw() # test2
+if __name__ == '__main__':  # 클래스를 정의한 파일내에서 작동하기위함
+    throw()
